1ce3f749d1a04a43803fd76f5f6fb8f0d65aa371
satoru.inagi <youmukeine@yahoo.co.jp>
2019-07-13 11:24:58 +0000

    6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:destroy, :show, :edit]
+  before_action :correct_user, only: [:destroy, :show, :edit, :update]
     
     def index
       if logged_in?
@@ -48,10 +47,6 @@ class TasksController < ApplicationController
     end
     
     private
-    
-    def set_task
-      @task = Task.find(params[:id])
-    end
     
     def task_params
       params.require(:task).permit(:content, :status)
